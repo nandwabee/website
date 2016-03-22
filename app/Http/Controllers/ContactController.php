0104416b4307716ea74940b80b6db26358780e7e
Bernard Nandwa <nandwabee@gmail.com>
2016-03-22 10:41:16 +0300

    Send an email whenever the contact form is submitted

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -1,14 +1,15 @@
 <?php namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mail;
 use Validator;
 
 class ContactController extends Controller{
     public function hire_me(Request $request){
         $validator = Validator::make($request->all(),[
             'email' => 'required|email|max:50',
-            'phone' => 'numeric|max:50',
-            'budget' => 'required|max:15',
+            'phone' => 'max:25',
+            'budget' => 'required|max:50',
             'name' => 'required|max:35',
             'message' => 'max:3000|required',
             'scope' => 'max:10|required|string',
@@ -24,6 +25,22 @@ class ContactController extends Controller{
         }
         else{
             //Send an email
+            $payload = [
+                'email_message' => $request->message,
+                'budget' => $request->budget,
+                'phone' => $request->phone,
+                'name' => $request->name,
+                'website' => $request->website,
+                'scope' => $request->scope
+            ];
+
+            $recipient = env('CONTACT_EMAIL');
+
+            Mail::send('emails.contact', $payload, function ($m) use ($recipient,$request) {
+                $m->from($request->email, $request->name);
+
+                $m->to($recipient, 'Bernard Nandwa')->subject('Hire Me Enquiry');
+            });
 
             //Show a thanks message
 
