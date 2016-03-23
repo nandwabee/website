96a050d01bff53b403774af17b8aad3c9b37ff97
Bernard Nandwa <nandwabee@gmail.com>
2016-03-23 13:07:55 +0300

    Catch all non http exceptions using one view.Todo:lets log this

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,1 +1,27 @@
-Opps
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#212121">
+
+    <!-- Styles -->
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Bernard Nandwa - Full Stack Developer</title>
+</head>
+<body class="grey darken-4" id="frontpage">
+<div class="container">
+    <div class="row welcome-row">
+        <div class="col s12 m12 l12">
+            <div class="title white-text">500</div>
+            <div class="white-text welcome-text">
+                <p>Lets <a href="/">start all over.</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script src="/js/app.js"></script>
+</body>
+</html>
