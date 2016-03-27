ef28a0bf013c821d49300db9dd508d4aa9b9532c
Bernard Nandwa <nandwabee@gmail.com>
2016-03-27 07:35:01 +0300

    Big blue edit button

--- resources/views/blogs/show.blade.php
+++ resources/views/blogs/show.blade.php
@@ -9,6 +9,10 @@
 
     <div class="blog-body">
         <div class="container">
+            <a class="btn-floating btn-large waves-effect waves-light blue edit-btn" href="/blog/{{$blog->nid}}/edit">
+                <i class="mdi mdi-pencil"></i>
+            </a>
+
             <div class="blog-numbers">
                 Published at
             </div>
